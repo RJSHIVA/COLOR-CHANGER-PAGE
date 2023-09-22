58afd2cc935a95a3091c7dae17bac1aff17e2cc2
RAHUL CHOUDHARY <111217626+RJSHIVA@users.noreply.github.com>
2023-09-22 06:33:23 +0000

    project done

---
+++ 03_script.js
@@ -0,0 +1,21 @@
+const buttons = document.querySelectorAll('.button')
+const body = document.querySelector('body')
+
+burrons.forEach(function (button) {
+    button.eventListner('click', function (e){
+
+        if (e.target.id === 'grey') {
+            body.style.backgroundColor = e.target.id;
+          }
+          if (e.target.id === 'white') {
+            body.style.backgroundColor = e.target.id;
+          }
+          if (e.target.id === 'blue') {
+            body.style.backgroundColor = e.target.id;
+          }
+          if (e.target.id === 'yellow') {
+            body.style.backgroundColor = e.target.id;
+          }
+          
+    })
+});
